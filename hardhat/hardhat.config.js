bfbd649d420a26e9326474dfc8ed6b287c9d3fb1
RS <rado.stanchev.bg@gmail.com>
2024-12-30 17:40:16 +0200

    deployment

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,13 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('@nomicfoundation/hardhat-ethers');
-require('@nomicfoundation/hardhat-ignition-ethers');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
-  networks:  {
-    devnet:  {
-      url: "http://localhost:8545",
+  solidity: "0.8.28", // You can adjust the version as necessary
+  networks: {
+    devnet: {
+      url: "http://localhost:8545",  // Ensure this matches the exposed port of Geth in Docker
     }
   }
 };
