439586f711f43777082275c6821708a3cc35a475
RS <rado.stanchev.bg@gmail.com>
2024-12-30 15:58:58 +0200

    adjust names

--- hardhat/contracts/MyToken.sol
+++ hardhat/contracts/MyToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Token {
+contract MyToken {
     string public name = "MyToken";
     string public symbol = "MTK";
     uint256 public totalSupply = 1000000;
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,17 +1,16 @@
 async function main() {
-    const [deployer] = await ethers.getSigners();
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy();
-    await token.deployed();
-    console.log("Token deployed to:", token.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-  
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const MyToken = await ethers.getContractFactory("MyToken");
+  const myToken = await MyToken.deploy();
+  await myToken.deployed();
+  console.log("MyToken deployed to:", myToken.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
