d5267d090cbfcec7c9f4166c27bc36af722189fe
RS <rado.stanchev.bg@gmail.com>
2024-12-31 15:12:17 +0200

    commit

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,10 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28", // You can adjust the version as necessary
+  solidity: "0.8.28",
   networks: {
     devnet: {
       url: "http://localhost:8545",
-    }
-  }
+    },
+  },
 };
